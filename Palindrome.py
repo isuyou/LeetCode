c8e3ed78d77e00c1d67c1215b92c64d43dccc4e2
isuyou <als211@case.edu>
2021-12-13 19:10:44 -0500

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -27,18 +27,52 @@ class Solution:
 
 
 
+#Manacher's Algorithm
 class Solution2:
     bogus='|'
 
     def __init__(self):
-        self.data = [bogus]
+        self.data = [self.bogus]
+        self.radii = []
         self.center=0
         self.radius=0
+        self.maxCenter=-1
+        self.maxRadius=-1
     
     def longestPalindrome(self,string=""):
         for i in range(len(string)):
-            self.data.extend([string[i],bogus])
-        for i in range(len(bogus))
-            compareCenter(string,i)
+            self.data.extend([string[i],self.bogus])
+        #initializing first element of radii for mirrored collection at element 0
+        self.radii.append(0)
+        for i in range(1,len(self.data)):
+            self.compareCenter(i)
+        left = (self.maxCenter-self.maxRadius)//2
+        right = (self.maxCenter+self.maxRadius+1)//2
+        s = string[left:right]
+        return self.radii
     
-    def compareCenter(self,string,index):
+    #Manacher's algorithm shortcut so you don't need to calculate mirrored non-edge cases
+    def compareCenter(self,index):
+        if index == 8:
+            print(self.data[8])
+        mirror = 2 * self.center - index
+        if index + self.radii[mirror] < self.center + self.radius:
+            self.radii.append(self.radii[mirror])
+        else:
+            if index < self.center + self.radius:
+                currentRadius = self.radii[self.center]//2
+            else:
+                currentRadius = 0
+            while (
+                index - currentRadius > -1 and index + currentRadius < len(self.data) and 
+                self.data[index-currentRadius] == self.data[index+currentRadius]
+            ):
+                currentRadius += 1
+            currentRadius -=1 
+            if index + currentRadius > self.center + self.radius:
+                self.center = index
+                self.radius = currentRadius
+                if self.radius > self.maxRadius:
+                    self.maxCenter = self.center
+                    self.maxRadius = self.radius
+            self.radii.append(currentRadius)
